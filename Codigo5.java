e0bc8eb6e1cb9263ef40e606fcb4c9fde21ade80
Raul Maldonado <r.maldonadop95@gmail.com>
2025-07-11 11:05:54 -0600

    revision 5

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,34 @@
+import java.util.Scanner;
+
 public class Codigo5 {
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
-	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
-	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
-			afo++;
-	      } else {
-			noAfo++;
-		  
-		  ni /= 10;
-	    }
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Introduzca un número: ");
+		String ni = s.nextLine();
 
-	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
-	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
-	  
+		int c = Integer.parseInt(ni);
+		int numero = c;
+
+		int afo = 0;
+		int noAfo = 0;
+
+		while (numero > 0) {
+			int digito = numero % 10;
+			if (digito == 3 || digito == 7 || digito == 8 || digito == 9) {
+				afo++;
+			} else {
+				noAfo++;
+			}
+			numero /= 10;
+		}
+
+		if (afo > noAfo) {
+			System.out.println("El " + c + " es un número afortunado.");
+		} else {
+			System.out.println("El " + c + " no es un número afortunado.");
+		}
+
+		s.close();
 	}
+}
